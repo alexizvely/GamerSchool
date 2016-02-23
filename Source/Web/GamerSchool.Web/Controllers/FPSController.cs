49e2e0452b3280a9a571147e6a3f4cec87c6f0a0
PavelNPavlov <pavelnapavlov@gmail.com>
2016-02-24 01:38:33 +0200

    Added contollers

---
+++ Source/Web/GamerSchool.Web/Controllers/FPSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamerSchool.Web.Controllers
+{
+    public class FPSController : Controller
+    {
+        // GET: FPS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Source/Web/GamerSchool.Web/Controllers/StrategyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamerSchool.Web.Controllers
+{
+    public class StrategyController : Controller
+    {
+        // GET: Strategy
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
