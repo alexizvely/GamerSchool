f802051b0eba856dea36d3f34e4b2c114e745a09
PavelNPavlov <pavelnapavlov@gmail.com>
2016-02-23 23:50:21 +0200

    Added address models

---
+++ Source/Data/GamerSchool.Data.Models/Address.cs
@@ -0,0 +1,32 @@
+﻿namespace GamerSchool.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Data.Common;
+    using Data.Common.Models;
+
+    public class Address : BaseModel<int>
+    {
+        public int CityId { get; set; }
+
+        public virtual City City { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+
+        [Required]
+        [MinLength(ValidationConstants.MinAddressLineLength, ErrorMessage = ValidationConstants.MinLengthErrorMessage)]
+        [MaxLength(ValidationConstants.MaxAddressLineLength, ErrorMessage = ValidationConstants.MaxLengthErrorMessage)]
+        public string Address { get; set; }
+
+        [Required]
+        [MinLength(ValidationConstants.MinTelephoneNumberLength, ErrorMessage = ValidationConstants.MinLengthErrorMessage)]
+        [MaxLength(ValidationConstants.MaxTelephoneNumberLength, ErrorMessage = ValidationConstants.MaxLengthErrorMessage)]
+        public string TelephoneNumber { get; set; }
+
+        [Required]
+        [MinLength(ValidationConstants.MinRecipientNamesLength, ErrorMessage = ValidationConstants.MinLengthErrorMessage)]
+        [MaxLength(ValidationConstants.MaxRecipientNamesLength, ErrorMessage = ValidationConstants.MaxLengthErrorMessage)]
+        public string RecipientNames { get; set; }
+    }
+}
---
+++ Source/Data/GamerSchool.Data.Models/City.cs
@@ -0,0 +1,15 @@
+﻿namespace GamerSchool.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Common;
+    using Common.Models;
+
+    public class City : BaseModel<int>
+    {
+        [Required]
+        [MinLength(ValidationConstants.MinCityNameLength, ErrorMessage = ValidationConstants.MinLengthErrorMessage)]
+        [MaxLength(ValidationConstants.MaxCityNameLength, ErrorMessage = ValidationConstants.MaxLengthErrorMessage)]
+        public string Name { get; set; }
+
+    }
+}
