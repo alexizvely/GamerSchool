8371364e28642a685152b47c7cad9262a49f5552
pavelnpavlov <pavelnapavlov@gmail.com>
2016-02-24 16:13:51 +0200

    readme and small updates

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# ASP.NET-MVC-Course-Project
-*
-# Course project for TA 2015
+# Gamer School
+
+```
+ASP.NET MVC course project for [`Telerik Academy`](http://telerikacademy.com)` 2015 year`, **course:** *ASP.NET MVC 2016*
+```
+
+> School for gamers with tutorials (fps and strategy), courses.
+
+## Used technologies and packages
+> - ASP.NET
+ - SQL Server (with EF)
+ - AJAX (Refreshing resource statistics on demand, leaderboards)
+ - KendoUI ASP.NET MVC Wrappers
+ - Dependency Injection with Autofac IoC container
+ - Automapper object mapping
+ - Caching and Error Handling ()
+
+## Built With
+
+>*  Visual Studio 2015 with Update 1
+* Atom
+
+## Author
+
+>* **Pavel Pavlov** - *more of my work:* - [PavelNPavlov](https://github.com/PavelNPavlov)
+
+## License
+
+>This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Images
+ >- Diagram
+ ![Db Diagram](/diagram.png)
+ - Screenshots
+ ![Home](/home.png)
+ ![Fps](/fps.PNG)
---
+++ Source/Web/GamerSchool.Web/Areas/Users/Controllers/CourseObjectiveController.cs
@@ -0,0 +1,90 @@
+﻿namespace GamerSchool.Web.Areas.Users.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using GamerSchool.Common;
+    using GamerSchool.Common.Extensions;
+    using GamerSchool.Data.Models;
+    using GamerSchool.Services.Data.Contracts;
+    using GamerSchool.Web.Controllers;
+    using GamerSchool.Web.Infrastructure.Constants;
+    using GamerSchool.Web.Infrastructure.Mapping;
+    using GamerSchool.Web.Infrastructure.UploadHelpers;
+    using GamerSchool.Web.ViewModels.GameCourse;
+
+    public class CourseObjectiveController : BaseController
+    {
+        private const int ItemsPerPage = 6;
+        private readonly ICoursePlansService objectives;
+        private readonly IUsersService users;
+
+        public CourseObjectiveController(ICoursePlansService objectives, IUsersService users)
+        {
+            this.objectives = objectives;
+            this.users = users;
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GameCourseInputModel model)
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var currentUserId = this.UserProfile.Id;
+                var newCourse = this.Mapper.Map<CoursePlan>(model);
+
+                newCourse.CreatorId = currentUserId;
+
+                var result = this.objectives.Create(newCourse);
+
+                return this.RedirectToAction("CourseObjective", "Academy", new { area = string.Empty, id = result });
+            }
+
+            return this.View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var art = this.objectives.GetById(id)
+            .To<GameCourseInputModel>()
+            .FirstOrDefault();
+
+            return this.View(art);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(GameCourseInputModel model)
+        {
+            if (model != null && this.ModelState.IsValid)
+            {
+                var currentUserId = this.UserProfile.Id;
+                var updatedCourse = this.Mapper.Map<CoursePlan>(model);
+
+                this.objectives.Update(model.Id, updatedCourse);
+
+                return this.RedirectToAction("CourseObjective", "Academy", new { area = string.Empty, id = model.Id });
+            }
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Destroy(int id)
+        {
+            this.objectives.Destroy(id, this.UserProfile.Id);
+
+            return this.RedirectToAction("Index", "Academy", new { area = "Users", id = 1 });
+        }
+    }
+}
